00f66782a9ecd45254c81cfd6f66602b2b257e9c
Kimsu <betnwjd25@gmail.com>
2022-11-03 16:50:06 +0900

    [김수정]Assignment1:Express초기환경세팅

--- sujeong/app.js
+++ sujeong/app.js
@@ -1,23 +1,12 @@
+require('dotenv').config() 
 const http = require('http');
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-const dotenv = require('dotenv');
-
-//모든 RES에 CORS요청을 설정하는법 
-// app.use(cors())
-// app.get('/ping', function (req, res, next){
-//     res.json({message:'pong'})
-// })
-// app.listen(3000, function(){
-//     console.log('server listening on port 3000')
-// })
-
-dotenv.config() //환경변수보다 위에 놓기
 
 const {dataSource, DataSource} = require('typeorm');
 const { runInNewContext } = require('vm');
-const myDataSource = new DataSource({
+const appDataSource = new DataSource({
     type: process.env.TYPEORM_CONNECTION,
     host: process.env.TYPEORM_HOST,
     port: process.env.TYPEORM_PORT,
@@ -26,13 +15,13 @@ const myDataSource = new DataSource({
     database: process.env.TYPEORM_DATABASE
 })
 
-myDataSource.initialize()
+appDataSource.initialize()
 .then(()=>{
     console.log("Data Source has been initialized!")
 })
 .catch((err)=>{
     console.error("Error during Data Source initialization",err)
-    myDataSource.destroy()
+    appDataSource.destroy()
 })
 
 app = express();
@@ -42,27 +31,12 @@ app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'))
 
-// app.get("ping", (req, res) =>{
-//     res.json({message: "pong"})
-// })
 
-//health check
 app.get("/ping",(req, res, next)=>{
     res.status(200).json({message:'pong'})
 })
 
-// app.post("/users",async(req , res, next)=>{
-//     const {name, password} =req.body
 
-//     await myDataSource.query(
-//         `INSERT INTO users(
-//             name,
-//             password
-//         )VALUES(?,?,?);`
-//         [name, password]
-//     );
-//     res.status(200).json({message: "userCreated"})
-// })
 
 
 const server = http.createServer(app)
