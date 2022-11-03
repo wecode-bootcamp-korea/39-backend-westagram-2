85381976e855acd146706c8abef500f05283e285
Kimsu <betnwjd25@gmail.com>
2022-11-03 21:43:39 +0900

    [김수정]Assignment1:Express초기환경세팅

--- sujeong/app.js
+++ sujeong/app.js
@@ -1,5 +1,6 @@
-require('dotenv').config() 
 const http = require('http');
+
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
