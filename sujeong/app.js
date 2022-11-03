7c1a7f0b1faffe5c1f161eeb7a434122a0bbc35a
Kimsu <betnwjd25@gmail.com>
2022-11-03 13:23:44 +0900

    [김수정]Westagram Backend Project - Express 초기 환경세팅

---
+++ sujeong/app.js
@@ -0,0 +1,64 @@
+const http = require('http');
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+
+//모든 RES에 CORS요청을 설정하는법 
+// app.use(cors())
+// app.get('/ping', function (req, res, next){
+//     res.json({message:'pong'})
+// })
+// app.listen(3000, function(){
+//     console.log('server listening on port 3000')
+// })
+
+dotenv.config() //환경변수보다 위에 놓기
+
+const {dataSource, DataSource} = require('typeorm');
+const { runInNewContext } = require('vm');
+const myDataSource = new DataSource({
+    type: process.env.TYPEORM_CONNECTION,
+    host: process.env.TYPEORM_HOST,
+    port: process.env.TYPEORM_PORT,
+    username: process.env.TYPEORM_USERNAME,
+    password: process.env.TYPEORM_PASSWORD,
+    database: process.env.TYPEORM_DATABASE
+})
+
+myDataSource.initialize()
+.then(()=>{
+    console.log("Data Source has been initialized!")
+})
+.catch((err)=>{
+    console.error("Error during Data Source initialization",err)
+    myDataSource.destroy()
+})
+
+app = express();
+const PORT = process.env.PORT
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'))
+
+app.get("ping", (req, res) =>{
+    res.json({message: "pong"})
+})
+
+//health check
+app.get("/ping",(req, res, next)=>{
+    res.status(200).json({message:'pong'})
+})
+
+const server = http.createServer(app)
+
+const start = async() => {
+    try{
+        app.listen(PORT, () => console.log(`Server is listening on  ${PORT}`));
+    } catch (err){
+        console.error(err);
+    }
+};
+
+start()
