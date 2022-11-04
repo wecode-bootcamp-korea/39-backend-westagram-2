c832f827700100c5ee8a85f92b8499b6360343cb
Kimsu <betnwjd25@gmail.com>
2022-11-04 15:03:43 +0900

    [김수정]Assignment 1: Express API -유저회원가입하기

--- sujeong/db/migrations/20221104021408_create_signup.sql
+++ sujeong/db/migrations/20221104021408_create_signup.sql
@@ -1,13 +1,15 @@
 -- migrate:up
-CREATE TABLE signup(
+CREATE TABLE users(    
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    profile_image VARCHAR (1000) NULL,
-    password VARCHAR(200) NOT NULL,
+    email VARCHAR(200) NOT NULL,
+    profile_image VARCHAR(1000) NULL,
+    password VARCHAR(200) 
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    ADD UNIQUE (email);
 );
 
+
 -- migrate:down
-DROP TABLE signup;
+DROP TABLE users;
